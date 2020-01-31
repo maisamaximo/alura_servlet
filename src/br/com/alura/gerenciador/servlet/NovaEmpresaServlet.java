3d20a2d499a90dea6aed989b3f76b66ba7e99078
MAISA MAXIMO FERREIRA <maisa.ferreira@LELLO.CORP>
2020-01-31 18:05:23 -0300

    first commit

--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -19,7 +19,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");		
 		String nomeEmpresa = request.getParameter("nome");
 		System.out.println(nomeEmpresa);
