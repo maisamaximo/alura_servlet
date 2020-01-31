1bb66f147f2e47c1682f07f3ff36df1fd9f5c3c1
MAISA MAXIMO FERREIRA <maisa.ferreira@LELLO.CORP>
2020-01-31 18:03:04 -0300

    first commit

---
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,35 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");		
+		String nomeEmpresa = request.getParameter("nome");
+		System.out.println(nomeEmpresa);
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<p> Empresa " + nomeEmpresa + " Cadastrada com sucesso </p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
